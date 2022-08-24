73e9f89d6c9c266e11ffd7137dae2649135aec19
bornvon <958580980@qq.com>
2022-08-24 23:53:36 +0800

    1.适配mediaStore

--- app/src/main/java/com/bornproduct/ultimatepiggy/utils/PictureSaveUtil.kt
+++ app/src/main/java/com/bornproduct/ultimatepiggy/utils/PictureSaveUtil.kt
@@ -2,55 +2,63 @@ package com.bornproduct.ultimatepiggy.utils
 
 import android.content.ContentValues
 import android.content.Context
-import android.content.Intent
+import android.graphics.Bitmap
 import android.media.MediaScannerConnection
 import android.net.Uri
 import android.os.Environment
+import android.os.Environment.DIRECTORY_PICTURES
 import android.provider.MediaStore
-import android.util.Log
-import com.bornproduct.ultimatepiggy.basic.MainApplication
 import com.bornproduct.ultimatepiggy.basic.log.Logger
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.io.InputStream
+import java.io.OutputStream
+import java.lang.Exception
 
 class PictureSaveUtil(private val context: Context) {
 
   companion object {
     private const val TAG = "PermissionUtil"
-    const val PICTURE_FILE_DIR_NAME = "SexyPiggy/"
+    const val PICTURE_FILE_DIR_NAME = "/SexyPiggy "
     const val MAX_DEAL_SIZE = 1024
-    const val DEFAULT_PICTURE_NAME = "defaultPicture.jpg"
+    const val DEFAULT_PICTURE_NAME = "defaultPicture"
+    const val MIME_TYPE = "image/png"
   }
 
   /**
    *  检查目录并创建默认图
    */
   fun checkFileDir(): File {
-    val baseDir = File(
-      Environment.getExternalStoragePublicDirectory(
-        Environment.DIRECTORY_PICTURES
-      ),
-      PICTURE_FILE_DIR_NAME
-    )
+    val baseDir = File(getFileDirPath())
     if (!baseDir.exists()) {
       // 目录不存在 则创建
       baseDir.mkdirs()
-      saveDefaultPicture(baseDir)
+      createDefaultFile()
     }
     return baseDir
   }
 
+  /**
+   * 获取本app相册文件夹绝对路径
+   */
+  private fun getFileDirPath(): String {
+    return File(
+      Environment.getExternalStoragePublicDirectory(
+        DIRECTORY_PICTURES
+      ),
+      PICTURE_FILE_DIR_NAME
+    ).path
+  }
 
   /**
    * 通知图片变更刷新
-   * @param fileNameArray 图片文件名数组
+   * @param fileNameArray 图片文件名数组(Environment.getExternalStoragePublicDirectory下)
    * @param scanListener 刷新回调
    */
   private fun notifyPictureRefresh(
     fileNameArray: Array<String>,
-    scanListener: (path: String, uri: Uri) -> Unit = { _, _ -> }
+    scanListener: (path: String?, uri: Uri?) -> Unit = { _, _ -> }
   ) {
     MediaScannerConnection.scanFile(
       context, fileNameArray, null
@@ -59,33 +67,31 @@ class PictureSaveUtil(private val context: Context) {
     }
   }
 
-
   /**
    * 创建默认图
-   * @param baseDir 根文件夹
    */
-  private fun saveDefaultPicture(baseDir: File) {
-    val defaultFile = File(baseDir, DEFAULT_PICTURE_NAME)
-    val fileOutputStream = FileOutputStream(defaultFile)
-    val inputStream = context.assets.open(DEFAULT_PICTURE_NAME)
-    val byteArray = ByteArray(MAX_DEAL_SIZE)
+  private fun createDefaultFile() {
+    val contentUri = MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL);
+    val contentValues = ContentValues()
+    val dateTaken = System.currentTimeMillis()
+    contentValues.put(MediaStore.Images.Media.DATE_TAKEN, dateTaken);
+    contentValues.put(MediaStore.Images.Media.IS_PRIVATE, 1);
+    contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, DEFAULT_PICTURE_NAME);
+    contentValues.put(MediaStore.Images.Media.MIME_TYPE,MIME_TYPE);
+    contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, DIRECTORY_PICTURES + PICTURE_FILE_DIR_NAME);
+    val dateAdded = System.currentTimeMillis();
+    contentValues.put(MediaStore.Images.Media.DATE_ADDED, dateAdded);
+    val dateModified = System.currentTimeMillis();
+    contentValues.put(MediaStore.Images.Media.DATE_MODIFIED, dateModified);
+    val insert = context.contentResolver.insert(contentUri, contentValues);
     try {
-      fileOutputStream.use { outputStream ->
-        inputStream.use { inputStream ->
-          while (true) {
-            val readLen = inputStream.read(byteArray)
-            if (readLen == -1) {
-              break
-            }
-            outputStream.write(byteArray, 0, readLen)
-          }
-        }
-      }
-
-      notifyPictureRefresh(arrayOf(defaultFile.toString()))
-    } catch (e: Throwable) {
+      val outputStream = insert?.let { context.contentResolver.openOutputStream(it) };
+      val bitmap = Bitmap.createBitmap(200, 200, Bitmap.Config.ARGB_8888);
+      bitmap.compress(Bitmap.CompressFormat.PNG, 90, outputStream)
+      outputStream?.write(33);
+      notifyPictureRefresh(arrayOf(getFileDirPath() + DEFAULT_PICTURE_NAME))
+    } catch (e: Exception) {
       Logger.e(TAG, "创建默认图异常 ：${e.message}")
     }
   }
-
 }
